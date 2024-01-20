e11a53e9b4108eda725b7333946095c567815dec
duytuan207 <88073690+duytuan207@users.noreply.github.com>
2024-01-20 14:44:58 +0700

    Update rdp.py

--- rdp.py
+++ rdp.py
@@ -1,16 +1,16 @@
-print("This script was created by thewickedkarma. Follow them on github https://github.com/thewickedkarma")
+print("This script was created by duytuan207. Follow them on github https://github.com/duytuan207")
 import os, subprocess
-username=input("Enter your username: ")
-password=input("Enter a password: ")
-print("remember this password as it will be used while providing superuser commands on your dektop \n creating your user....have some patience")
+username=input("Nhập tên người dùng của bạn:")
+password=input("Gõ mật khẩu:")
+print("Hãy nhớ mật khẩu này vì nó sẽ được sử dụng trong khi cung cấp các lệnh siêu người dùng trên máy tính của bạn \n khi tạo người dùng của bạn....hãy kiên nhẫn một chút")
 os.system(f"useradd -m {username}")
 os.system(f"adduser {username} sudo")
 os.system(f"echo '{username}:{password}' | sudo chpasswd")
 os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
-print(f"User created and configured having username `{username}` and password `{password}`")
-CRP= input("Paste your Chrome Remote Desktop Command you had copied: ")
-Pin=input("Choose a pin for your chrome RDP(minimum 6 digits): ")
-print("Explore my youtube channel while it completes installing the RDP:  https://www.youtube.com/channel/UC-w_mhTHR6-Lc28PSiv7kaQ/videos")
+print(f"Người dùng được tạo và định cấu hình có tên người dùng `{username}` và mật khẩu `{password}`")
+CRP= input("Dán Lệnh Chrome Remote Desktop mà bạn đã sao chép:")
+Pin=input("Chọn mã pin cho RDP chrome của bạn (tối thiểu 6 chữ số):")
+print("Github tác giả: github.com/duytuan207")
 Autostart = True
 class CRD:
     def __init__(self, user):
@@ -19,10 +19,10 @@ class CRD:
         self.installDesktopEnvironment()
         self.installGoogleChorme()
         self.finish(user)
-        print("\nRDP created succesfully move to https://remotedesktop.google.com/access")
+        print("\nRDP được tạo thành công chuyển sang https://remotedesktop.google.com/access")
     @staticmethod
     def installCRD():
-        print("Installing Chrome Remote Desktop")
+        print("Tải Chrome Remote Desktop")
         subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
         subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
         subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
@@ -39,12 +39,15 @@ class CRD:
 
     @staticmethod
     def installGoogleChorme():
-        print("Installing Google Chrome")
+        print("Tải Google Chrome")
         subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
         subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
         subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
         os.system("wget https://dl1.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTUzMDc3NDYwMCwidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiIzM2ExNWQ3YWU0MTNhMTE2ZWJlY2E4YzZmOWFmYjhkNTE0Y2NmMmU0Y2U3ZTUzZDUyOWU0NDQ5YjY1N2Y3YmQ3MjMxNTIwZDRlZGE5ZjhiMWRmYjZiOTQ2Y2ZiNzU0OGJiODFhYjcxNTZhYTZiYWQ4M2UwYjA2MmEyMTY3NzcyNCIsInQiOjE2NDU1MTM0OTcsInN0ZnAiOiIyNGFhYWZiMWVhZmU2ZjU1YzUwMTI3ZmViMGJmOTdkNCIsInN0aXAiOiIzNS4xNTQuMTY2LjE1NSJ9.pfHVPOG-fhRwKRsVdNWiyaoepEIsw-3wqAosQcbwAE4/ocs-url_3.1.0-0ubuntu1_amd64.deb")
         os.system("dpkg -i ocs-url_3.1.0-0ubuntu1_amd64.deb")
+        os.system("apt-get install -y curl gnupg wget htop sudo git git-lfs software-properties-common build-essential libgl1 zip unzip")
+        os.system("curl -sL https://deb.nodesource.com/setup_18.x")
+        os.system("sudo apt install nodejs -y")
         os.system("sudo apt install -f -y")
 
     @staticmethod
@@ -52,7 +55,7 @@ class CRD:
         print("Finalizing")
         if Autostart:
             os.makedirs(f"/home/{user}/.config/autostart", exist_ok=True)
-            link = "https://github.com/thewickedkarma/"
+            link = "https://github.com/duytuan207/"
             colab_autostart = """[Desktop Entry]
 
 Type=Application
@@ -74,15 +77,15 @@ X-GNOME-Autostart-enabled=true""".format(link)
         os.system("service chrome-remote-desktop start")
         
 
-        print("Finished Succesfully")
-        print("RDP created succesfully move to https://remotedesktop.google.com/access")
+        print("Đã thành công!")
+        print("RDP tạo thành công và chuyển về https://remotedesktop.google.com/access")
 
 try:
     if CRP == "":
-        print("Please enter authcode from the given link")
+        print("Vui lòng nhập mã xác thực từ liên kết đã cho")
     elif len(str(Pin)) < 6:
-        print("Enter a pin more or equal to 6 digits")
+        print("Nhập mật khẩu ít nhất 6 chữ số")
     else:
         CRD(username)
 except NameError as e:
-    print("'username' variable not found, Create a user first")
+    print("'username' không thấy, tạo người dùng trước...")
